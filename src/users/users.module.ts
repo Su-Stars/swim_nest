5e55b1d673e4e9cd095329b5eb9dfb418a24fc1d
damhyeong <rhdwhdals8@naver.com>
2025-01-13 23:17:35 +0900

    실수로 변경사항 집어넣음

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,6 +6,6 @@ import { UsersService } from "./users.service";
   imports : [],
   controllers : [UsersController],
   providers : [UsersService],
-  exports : []
+  exports : [UsersService]
 })
 export class UsersModule {}
