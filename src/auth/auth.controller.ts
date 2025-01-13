f76fc41154c01a1dce1433603e7983d8fca9ad35
damhyeong <rhdwhdals8@naver.com>
2025-01-13 23:17:35 +0900

    실수로 변경사항 집어넣음

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUsersDto } from "./dto/create-users.dto";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService) {
+  }
+
+  @Post("login")
+  async login(@Body() createUsersDto : CreateUsersDto) {
+  }
+}
