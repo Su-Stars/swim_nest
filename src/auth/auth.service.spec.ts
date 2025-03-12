737f397ec7b0c4a98c71a8c2659782a3e5002f39
damhyeong <rhdwhdals8@naver.com>
2025-03-12 23:56:49 +0900

    [Unit test] : 유닛 테스트 생성

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -15,4 +15,8 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('service 메서드 검증', () => {
+    expect(service.generateAccessToken({id : 1, email : "sample@gmail.com"})).toBe(String);
+  })
 });
